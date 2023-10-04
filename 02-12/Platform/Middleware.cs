8a0ebc00d392fba72a0adbc12650707fe1ef83f6
woodeneels <woodeneels@users.noreply.github.com>
2023-10-04 17:00:07 +0100

    progress chapter 12

---
+++ 02-12/Platform/Middleware.cs
@@ -0,0 +1,19 @@
+﻿namespace Platform {
+    public class QueryStringMiddleware {
+        private RequestDelegate next;
+
+        public QueryStringMiddleware(RequestDelegate nextDelegate) {
+            next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context) {
+            if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true") {
+                if (!context.Response.HasStarted) {
+                    context.Response.ContentType = "text/plain";
+                }
+                await context.Response.WriteAsync("Class-based middleware\n");
+            }
+            await next(context);
+        }
+    }
+}
